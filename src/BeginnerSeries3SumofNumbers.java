612a636fb67a0d9c88aed69e135e3671e6912c38
Lloyd Perez <lloyd4965jr@yahoo.com>
2023-01-25 17:44:23 -0500

    BeginnerSeriers3SumofNumbers

---
+++ src/BeginnerSeries3SumofNumbers.java
@@ -0,0 +1,14 @@
+package src;
+
+public class BeginnerSeries3SumofNumbers {
+    public int GetSum(int a, int b) {
+
+        if (a == b) {
+            return a;
+        } else if (a > b) {
+            return a + GetSum(a - 1, b);
+        } else {
+            return a + GetSum(a + 1, b);
+        }
+    }
+}
