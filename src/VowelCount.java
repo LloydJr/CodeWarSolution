3b7d829144a87a7f078ad24e14c788405d80afe7
Lloyd Perez <lloyd4965jr@yahoo.com>
2023-01-25 17:49:50 -0500

    YoureASquare

--- src/VowelCount.java
+++ src/VowelCount.java
@@ -1,4 +1,19 @@
 package src;
 
 public class VowelCount {
+    public static int getCount(String str) {
+        int count = 0;
+
+        char[] charArray = str.toCharArray();
+        for(int i = 0; i < charArray.length; i++){
+            if (charArray[i] == ('a') ||
+                    charArray[i] == ('e') ||
+                    charArray[i] == ('i') ||
+                    charArray[i] == ('o') ||
+                    charArray[i] == ('u')){
+                count++;
+            }
+        }
+        return count;
+    }
 }
---
+++ src/YoureASquare.java
@@ -0,0 +1,7 @@
+package src;
+
+public class YoureASquare {
+    public static boolean isSquare(int n) {
+        return Math.sqrt(n) == (int) Math.sqrt(n);
+    }
+}
