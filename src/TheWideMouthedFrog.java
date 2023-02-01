fcf95b3e9ae1a732cad9182c1d17cb5949592fc2
Lloyd Perez <lloyd4965jr@yahoo.com>
2023-02-01 09:53:14 -0500

    TheWideMouthedFrog

---
+++ src/TheWideMouthedFrog.java
@@ -0,0 +1,8 @@
+package src;
+
+public class TheWideMouthedFrog {
+    public static String mouthSize(String animal){
+        return animal.toLowerCase().contains("alligator") ? "small" : "wide";
+    }
+
+}
