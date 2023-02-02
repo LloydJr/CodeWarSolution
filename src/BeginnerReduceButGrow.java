8af58bb82022bd999ce6955ceef148bf9d830954
Lloyd Perez <lloyd4965jr@yahoo.com>
2023-02-02 01:51:38 -0500

    BeginnerReduceButGrow

---
+++ src/BeginnerReduceButGrow.java
@@ -0,0 +1,12 @@
+package src;
+
+public class BeginnerReduceButGrow {
+    public static int grow(int[] x){
+        int sum = 1;
+
+        for(int i = 0; i < x.length; i++){
+            sum *= x[i];
+        }
+        return sum;
+    }
+}
